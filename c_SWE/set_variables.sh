58730798dbb238f5381015533497dcf1fc1e9cb3
tuanta-ucar <tqta@ucar.edu>
2015-06-16 12:32:17 -0600

    A commit

--- c_SWE/set_variables.sh
+++ c_SWE/set_variables.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export KMP_AFFINITY=verbose,granularity=thread,compact,1,0 # 1 thread per core first
+#export KMP_AFFINITY=verbose,granularity=thread,compact,1,0 # 1 thread per core first
 #export KMP_AFFINITY=verbose,granularity=thread,compact      # 2 threads per core first
 
-#export KMP_AFFINITY=verbose,granularity=thread,scatter
+export KMP_AFFINITY=verbose,granularity=thread,scatter
 
 #export OMP_NUM_THREADS=1
 #./swe
